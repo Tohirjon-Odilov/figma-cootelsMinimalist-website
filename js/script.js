26d78706a0a524b17cd3ef36b394bcf6c23f40db
Tohirjon-Odilov <shahanshoh819@gmail.com>
2022-05-21 12:19:04 +0500

    navbar update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 let bars = document.querySelector('#bars')
-document.body.onclick = () => {
+let x = document.querySelector('#x')
+    // document.body.onclick = () => {
+    //     navbar.classList.toggle('active')
+    //     x.classList.toggle('active')
+    //     console.log('Navbar is active')
+    //     bars.style.display = 'none'
+    // }
+bars.onclick = () => {
     navbar.classList.toggle('active')
-    console.log('Navbar is active')
-
+        // x.classList.toggle('active')
+        // console.log('Navbar is active')
+    bars.style.display = 'none'
+    x.style.display = 'block'
+}
+x.onclick = () => {
+    navbar.classList.remove('active')
+        // x.classList.toggle('active')
+        // console.log('Navbar is active')
+    x.style.display = 'none'
+    bars.style.display = 'block'
 }
